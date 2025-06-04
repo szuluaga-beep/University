c9365de95a3acd5b35d6f0d1c14d2beb41a8efc4
szuluaga-beep <steven000069@hotmail.com>
2025-06-04 13:19:00 -0500

    Add Course, Enrollment, and Student models with required properties

---
+++ Models/Course.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.Models;
+
+public class Course
+{
+
+    public int Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    public int Credits { get; set; }
+
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace University.Models;
+
+public class Enrollment
+{
+
+    public int EnrollmentID { get; set; }
+    public int CourseID { get; set; }
+    public int StudentID { get; set; }
+
+    public required Course Course { get; set; }
+    public required Student Student { get; set; }
+
+
+}
